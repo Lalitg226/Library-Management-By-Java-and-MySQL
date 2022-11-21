21ca51755ad3d7665d813591e967cdf99f640187
Lalitg226 <96005269+Lalitg226@users.noreply.github.com>
2022-11-21 20:58:29 +0530

    Add files via upload

---
+++ lib2/src/Conn.java
@@ -0,0 +1,21 @@
+//package libraryManagementSystem;
+
+import java.sql.*;
+
+public class Conn {
+	Connection c;
+	Statement s;
+
+	public Conn() {
+		try {
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+			c = DriverManager.getConnection("jdbc:mysql://localhost:3306/lib","root","#aws@225awesome");
+			s = c.createStatement();
+			
+		} catch(Exception e){
+			System.out.println(e); 
+		}
+	}
+	// sir u r not audible
+	
+}
